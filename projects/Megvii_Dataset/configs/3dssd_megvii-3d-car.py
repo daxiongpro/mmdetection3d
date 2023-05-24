d336459561eb1c69fda4534155eb69146f002d5c
daxiongpro <910660298@qq.com>
2023-05-24 13:27:36 +0800

    训练成功

--- projects/Megvii_Dataset/configs/3dssd_megvii-3d-car.py
+++ projects/Megvii_Dataset/configs/3dssd_megvii-3d-car.py
@@ -1,7 +1,7 @@
-_base_ = [
-    # 'mmdet3d::_base_/default_runtime.py',
-    '../../../configs/3dssd/3dssd_4xb4_kitti-3d-car.py'
-]
+# _base_ = [
+#     # 'mmdet3d::_base_/default_runtime.py',
+#     '../../../configs/3dssd/3dssd_4xb4_kitti-3d-car.py'
+# ]
 
 custom_imports = dict(
     imports=[
@@ -12,40 +12,102 @@ custom_imports = dict(
 
 
 model = dict(
+    type='SSD3DNet',
+    data_preprocessor=dict(type='Det3DDataPreprocessor'),
     backbone=dict(
-        in_channels=3),
+        type='PointNet2SAMSG',
+        in_channels=3,
+        num_points=(4096, 512, (256, 256)),
+        radii=((0.2, 0.4, 0.8), (0.4, 0.8, 1.6), (1.6, 3.2, 4.8)),
+        num_samples=((32, 32, 64), (32, 32, 64), (32, 32, 32)),
+        sa_channels=(((16, 16, 32), (16, 16, 32), (32, 32, 64)),
+                     ((64, 64, 128), (64, 64, 128), (64, 96, 128)),
+                     ((128, 128, 256), (128, 192, 256), (128, 256, 256))),
+        aggregation_channels=(64, 128, 256),
+        fps_mods=('D-FPS', 'FS', ('F-FPS', 'D-FPS')),
+        fps_sample_range_lists=(-1, -1, (512, -1)),
+        norm_cfg=dict(type='BN2d', eps=0.001, momentum=0.1),
+        sa_cfg=dict(
+            type='PointSAModuleMSG',
+            pool_mod='max',
+            use_xyz=True,
+            normalize_xyz=False)),
     bbox_head=dict(
+        type='SSD3DHead',
+        vote_module_cfg=dict(
+            in_channels=256,
+            num_points=256,
+            gt_per_seed=1,
+            conv_channels=(128, ),
+            conv_cfg=dict(type='Conv1d'),
+            norm_cfg=dict(type='BN1d', eps=0.001, momentum=0.1),
+            with_res_feat=False,
+            vote_xyz_range=(3.0, 3.0, 2.0)),
+        vote_aggregation_cfg=dict(
+            type='PointSAModuleMSG',
+            num_point=256,
+            radii=(4.8, 6.4),
+            sample_nums=(16, 32),
+            mlp_channels=((256, 256, 256, 512), (256, 256, 512, 1024)),
+            norm_cfg=dict(type='BN2d', eps=0.001, momentum=0.1),
+            use_xyz=True,
+            normalize_xyz=False,
+            bias=True),
+        pred_layer_cfg=dict(
+            in_channels=1536,
+            shared_conv_channels=(512, 128),
+            cls_conv_channels=(128, ),
+            reg_conv_channels=(128, ),
+            conv_cfg=dict(type='Conv1d'),
+            norm_cfg=dict(type='BN1d', eps=0.001, momentum=0.1),
+            bias=True),
+        objectness_loss=dict(
+            type='mmdet.CrossEntropyLoss',
+            use_sigmoid=True,
+            reduction='sum',
+            loss_weight=1.0),
+        center_loss=dict(
+            type='mmdet.SmoothL1Loss', reduction='sum', loss_weight=1.0),
+        dir_class_loss=dict(
+            type='mmdet.CrossEntropyLoss', reduction='sum', loss_weight=1.0),
+        dir_res_loss=dict(
+            type='mmdet.SmoothL1Loss', reduction='sum', loss_weight=1.0),
+        size_res_loss=dict(
+            type='mmdet.SmoothL1Loss', reduction='sum', loss_weight=1.0),
+        corner_loss=dict(
+            type='mmdet.SmoothL1Loss', reduction='sum', loss_weight=1.0),
+        vote_loss=dict(
+            type='mmdet.SmoothL1Loss', reduction='sum', loss_weight=1.0),
         num_classes=19,
         bbox_coder=dict(
-            type='AnchorFreeBBoxCoder', num_dir_bins=12, with_rot=True)))
-
-
-# dataset settings
+            type='AnchorFreeBBoxCoder', num_dir_bins=12, with_rot=True)),
+    train_cfg=dict(
+        sample_mode='spec', pos_distance_thr=10.0, expand_dims_length=0.05),
+    test_cfg=dict(
+        nms_cfg=dict(type='nms', iou_thr=0.1),
+        sample_mode='spec',
+        score_thr=0.0,
+        per_class_proposal=True,
+        max_output_num=100))
 dataset_type = 'MegviiDataset'
-ann_file = 'megvii_infos_train.pkl'
 data_root = 'data/kuangshi_data/ppl_bag_20220909_132234_det/'
-
+ann_file = 'megvii_infos_train.pkl'
+launcher = 'none'
+work_dir = './work_dirs/3dssd_megvii-3d-car'
 class_names = [
-    "小汽车", "汽车", "货车", "工程车", "巴士", "摩托车", "自行车", "三轮车", "骑车人", "骑行的人", "人",
-    "行人", "其它", "残影", "蒙版", "其他", "拖挂", "锥桶", "防撞柱"
+    '小汽车', '汽车', '货车', '工程车', '巴士', '摩托车', '自行车', '三轮车', '骑车人', '骑行的人', '人',
+    '行人', '其它', '残影', '蒙版', '其他', '拖挂', '锥桶', '防撞柱'
 ]
 
 point_cloud_range = [0, -40, -5, 70, 40, 3]
-db_sampler = dict(_delete_=True)
-val_evaluator = dict(type='MegviiMetric', ann_file=data_root + ann_file)
-test_evaluator = dict(type='MegviiMetric', ann_file=data_root + ann_file)
-metainfo = dict(_delete_=True, class_names=class_names)
-
+input_modality = dict(use_lidar=True, use_camera=False)
+metainfo = dict(class_names=class_names)
+db_sampler = dict()
 train_pipeline = [
-    dict(
-        type='MegviiLoadPointsFromFile',
-        # type='LoadPointsFromFile',
-        coord_type='LIDAR',
-        use_dim=3),
-    dict(
-        type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
-    dict(type='PointsRangeFilter', point_cloud_range=point_cloud_range),
-    dict(type='ObjectRangeFilter', point_cloud_range=point_cloud_range),
+    dict(type='MegviiLoadPointsFromFile', coord_type='LIDAR', use_dim=3),
+    dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
+    dict(type='PointsRangeFilter', point_cloud_range=[0, -40, -5, 70, 40, 3]),
+    dict(type='ObjectRangeFilter', point_cloud_range=[0, -40, -5, 70, 40, 3]),
     dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5),
     dict(
         type='ObjectNoise',
@@ -57,17 +119,17 @@ train_pipeline = [
         type='GlobalRotScaleTrans',
         rot_range=[-0.78539816, 0.78539816],
         scale_ratio_range=[0.9, 1.1]),
-    # 3DSSD can get a higher performance without this transform
-    # dict(type='BackgroundPointsFilter', bbox_enlarge_range=(0.5, 2.0, 0.5)),
     dict(type='PointSample', num_points=16384),
     dict(
         type='Pack3DDetInputs',
         keys=['points', 'gt_bboxes_3d', 'gt_labels_3d'])
 ]
-
 test_pipeline = [
-    dict(type='MegviiLoadPointsFromFile', coord_type='LIDAR', load_dim=3, use_dim=3),
-    # dict(type='LoadPointsFromFile', coord_type='LIDAR', load_dim=3, use_dim=3),
+    dict(
+        type='MegviiLoadPointsFromFile',
+        coord_type='LIDAR',
+        load_dim=3,
+        use_dim=3),
     dict(
         type='MultiScaleFlipAug3D',
         img_scale=(1333, 800),
@@ -77,40 +139,114 @@ test_pipeline = [
             dict(
                 type='GlobalRotScaleTrans',
                 rot_range=[0, 0],
-                scale_ratio_range=[1., 1.],
+                scale_ratio_range=[1.0, 1.0],
                 translation_std=[0, 0, 0]),
             dict(type='RandomFlip3D'),
             dict(
-                type='PointsRangeFilter', point_cloud_range=point_cloud_range),
-            dict(type='PointSample', num_points=16384),
+                type='PointsRangeFilter',
+                point_cloud_range=[0, -40, -5, 70, 40, 3]),
+            dict(type='PointSample', num_points=16384)
         ]),
     dict(type='Pack3DDetInputs', keys=['points'])
 ]
-
+eval_pipeline = [
+    dict(type='MegviiLoadPointsFromFile', coord_type='LIDAR', load_dim=3, use_dim=3),
+    dict(type='Pack3DDetInputs', keys=['points'])
+]
 train_dataloader = dict(
     batch_size=16,
     num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
+        type='RepeatDataset',
+        times=2,
         dataset=dict(
-            type=dataset_type,
+            type='MegviiDataset',
             data_root=data_root,
             ann_file=ann_file,
+            data_prefix=dict(pts='training/velodyne_reduced'),
             pipeline=train_pipeline,
-            metainfo=metainfo)))
-test_dataloader = dict(
-    dataset=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file=ann_file,
-        pipeline=test_pipeline,
-        metainfo=metainfo))
-val_dataloader = dict(
-    dataset=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file=ann_file,
-        pipeline=test_pipeline,
-        metainfo=metainfo))
-
-# test_dataloader = dict(_delete_=True)
-# val_dataloader = dict(_delete_=True)
+            modality=dict(use_lidar=True, use_camera=False),
+            test_mode=False,
+            metainfo=dict(class_names=class_names),
+            box_type_3d='LiDAR')))
+# val_dataloader = dict(
+#     batch_size=1,
+#     num_workers=1,
+#     persistent_workers=True,
+#     drop_last=False,
+#     sampler=dict(type='DefaultSampler', shuffle=False),
+#     dataset=dict(
+#         type='MegviiDataset',
+#         data_root=data_root,
+#         data_prefix=dict(pts='training/velodyne_reduced'),
+#         ann_file=ann_file,
+#         pipeline=eval_pipeline,
+#         modality=dict(use_lidar=True, use_camera=False),
+#         test_mode=True,
+#         metainfo=dict(class_names=class_names),
+#         box_type_3d='LiDAR'))
+# test_dataloader = dict(
+#     batch_size=1,
+#     num_workers=1,
+#     persistent_workers=True,
+#     drop_last=False,
+#     sampler=dict(type='DefaultSampler', shuffle=False),
+#     dataset=dict(
+#         type='MegviiDataset',
+#         data_root=data_root,
+#         data_prefix=dict(pts='training/velodyne_reduced'),
+#         ann_file=ann_file,
+#         pipeline=test_pipeline,
+#         modality=dict(use_lidar=True, use_camera=False),
+#         test_mode=True,
+#         metainfo=dict(class_names=class_names),
+#         box_type_3d='LiDAR'))
+# val_evaluator = dict(
+#     type='KittiMetric',
+#     ann_file=data_root + ann_file,
+#     metric='bbox')
+# test_evaluator = dict(
+#     type='KittiMetric',
+#     ann_file=data_root + ann_file,
+#     metric='bbox')
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    type='Det3DLocalVisualizer',
+    vis_backends=[dict(type='LocalVisBackend')],
+    name='visualizer')
+default_scope = 'mmdet3d'
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', interval=-1),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='Det3DVisualizationHook'))
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'))
+log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
+log_level = 'INFO'
+load_from = None
+resume = False
+file_client_args = dict(backend='disk')
+lr = 0.002
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=lr, weight_decay=0.0),
+    clip_grad=dict(max_norm=35, norm_type=2))
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=80, val_interval=20)
+# val_cfg = dict(type='ValLoop')
+# test_cfg = dict(type='TestLoop')
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=80,
+        by_epoch=True,
+        milestones=[45, 60],
+        gamma=0.1)
+]
